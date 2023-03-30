ad57e7288dbfdc8a6e8e9b99a3f6148c57b20d52
Zhaoy10 <zhaoy10@rose-hulman.edu>
2023-03-29 21:22:02 -0400

    finishe input field

--- CSSE352_UIProj/Assets/Scripts/LoginSample.cs
+++ CSSE352_UIProj/Assets/Scripts/LoginSample.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
 
 public class LoginSample : MonoBehaviour
 {
     public TMP_InputField Account;
     public TMP_InputField Password;
+    public TMP_InputField Email
+        ;
     // Start is called before the first frame update
     void Start()
     {
         Account = GameObject.Find("UserName").GetComponent<TMP_InputField>();
         Password = GameObject.Find("PassWord").GetComponent<TMP_InputField>();
+        Email = GameObject.Find("Email").GetComponent<TMP_InputField>();
+        Email.onValueChanged.AddListener(OnValidate);
+        Email.onEndEdit.AddListener(Ended);
     }
 
     // Update is called once per frame
@@ -19,5 +26,18 @@ public class LoginSample : MonoBehaviour
     {
         Debug.Log("UserName: " + Account.text);
         Debug.Log("PassWord: " + Password.text);
+
+    }
+
+
+
+    public void OnValidate(string text)
+    {
+
+        Debug.Log("Typing right now");
+    }
+    public void Ended(string text)
+    {
+        Debug.Log("Typing is finished");
     }
 }
